278291e3c436c618072886f9c1a4bb7bcaf1d4ef
Heniboj <heniboj@gmail.com>
2020-11-02 18:20:21 +0100

    Fájlok átrendezése

--- Programkod/cmake-build-debug/CMakeFiles/NagyHazi.dir/DependInfo.cmake
+++ Programkod/cmake-build-debug/CMakeFiles/NagyHazi.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "C:/Users/beren/Desktop/NagyHazi/main.c" "C:/Users/beren/Desktop/NagyHazi/cmake-build-debug/CMakeFiles/NagyHazi.dir/main.c.obj"
+  "C:/Users/beren/Desktop/NagyHazi/Programkod/main.c" "C:/Users/beren/Desktop/NagyHazi/Programkod/cmake-build-debug/CMakeFiles/NagyHazi.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 

d8bc0363444f9064f3728417d744b9a2e7e4dad3
Heniboj <heniboj@gmail.com>
2020-11-03 22:00:37 +0100

    Test case-ek, járatkeresés véglegesítése és foglalasok.txt beolvasása.

--- Programkod/main.c
+++ Programkod/main.c
@@ -15,12 +15,35 @@ typedef struct Datum {
 
 typedef struct Jarat {
   char azonosito[7];
-  Datum idopont;
+  Datum datum;
   char honnan[50];
   char hova[50];
   int ferohely;
+  int *foglaltUlesek;
 }Jarat;
 
+typedef enum Menu{
+    normal,
+    vega,
+    laktozmentes
+}Menu;
+
+typedef struct Foglalas {
+    char azonosito[7];
+    char nev[50];
+    int ulohely;
+    int menu;
+}Foglalas;
+
+Datum datumBeolvas(void) {
+    Datum datum;
+    char datumstring[11];
+    scanf("%s", datumstring);
+    datum.ev = atoi(strtok(datumstring, "/"));
+    datum.honap = atoi(strtok(NULL, "/"));
+    datum.nap = atoi(strtok(NULL, "/"));
+    return datum;
+}
 
 bool datumOsszehasonlit(Datum datum1, Datum datum2) {
     if(datum1.ev < datum2.ev) {
@@ -55,13 +78,13 @@ bool hibaKeres(Datum datum) {
 }
 
 
-void jaratKeres(Jarat *jaratok, int meret, char *honnan, char *hova, Datum idopont_kezdo, Datum idopont_vegso) {
+void jaratKeres(Jarat *jaratok, int meret, char *honnan, char *hova, Datum datum_kezdo, Datum datum_vegso) {
     int talalatok = 0;
     for(int i = 0; i < meret;i++) {
         if(strcmp(jaratok[i].honnan, honnan) == 0 && strcmp(jaratok[i].hova, hova) == 0) {
-            if(datumOsszehasonlit(jaratok[i].idopont, idopont_kezdo)) {
-                if(!datumOsszehasonlit(jaratok[i].idopont, idopont_vegso)) {
-                    printf("%s %d %d %d %s %s %d\n", jaratok[i].azonosito, jaratok[i].idopont.ev, jaratok[i].idopont.honap, jaratok[i].idopont.nap, jaratok[i].honnan, jaratok[i].hova, jaratok[i].ferohely);
+            if(datumOsszehasonlit(jaratok[i].datum, datum_kezdo)) {
+                if(!datumOsszehasonlit(jaratok[i].datum, datum_vegso)) {
+                    printf("%s %d %d %d %s %s %d\n", jaratok[i].azonosito, jaratok[i].datum.ev, jaratok[i].datum.honap, jaratok[i].datum.nap, jaratok[i].honnan, jaratok[i].hova, jaratok[i].ferohely);
                     talalatok++;
                 }
             }
@@ -72,6 +95,9 @@ void jaratKeres(Jarat *jaratok, int meret, char *honnan, char *hova, Datum idopo
     }
 }
 
+void jaratFoglal(char* jaratszam) {
+
+}
 
 
 int main() {
@@ -80,7 +106,7 @@ int main() {
     SetConsoleOutputCP(1250);
 #endif
 
-
+    /* jaratok.txt adatainak beolvasása és tárolása */
     FILE* fp;
     fp = fopen("jaratok.txt", "r");
     Jarat tempJarat;
@@ -89,31 +115,59 @@ int main() {
     int jaratokMeret = 1;
     Jarat* jaratok = (Jarat*)malloc(jaratokMeret * sizeof(*jaratok));
 
-    while(fscanf(fp,"%[^#]#%d/%d/%d#%[^#]#%[^#]#%d\n", tempJarat.azonosito, &(tempJarat.idopont.ev), &(tempJarat.idopont.honap), &(tempJarat.idopont.nap), tempJarat.honnan, tempJarat.hova, &(tempJarat.ferohely)) != EOF) {
+    while(fscanf(fp,"%[^#]#%d/%d/%d#%[^#]#%[^#]#%d\n", tempJarat.azonosito, &(tempJarat.datum.ev), &(tempJarat.datum.honap), &(tempJarat.datum.nap), tempJarat.honnan, tempJarat.hova, &(tempJarat.ferohely)) != EOF) {
+        tempJarat.foglaltUlesek = NULL;
         jaratok = (Jarat*)realloc(jaratok, jaratokMeret * sizeof(*jaratok));
         jaratok[i] = tempJarat;
         i++;
         jaratokMeret++;
     }
     jaratokMeret--;
-
-  /*  for(int j = 0; j < jaratokMeret; j++) {
-        printf("%s %d %d %d %s %s %d\n", jaratok[j].azonosito, jaratok[j].idopont.ev, jaratok[j].idopont.honap, jaratok[j].idopont.nap, jaratok[j].honnan, jaratok[j].hova, jaratok[j].ferohely);
-    }*/
-
-  Datum ido1 = {2019, 12, 20};
-  Datum ido2 = {2021, 12, 21};
-
-  //jaratKeres(jaratok, jaratokMeret, "Budapest", "New York city", ido1, ido2);
+    fclose(fp);
 
 
+    /* foglalasok.txt adatainaka beolvasása és tárolása */
+    fp = fopen("foglalasok.txt", "r");
+    Foglalas tempFoglalas;
+
+    i = 0;
+    int foglalasokMeret = 0;
+    Foglalas* foglalasok = NULL;
+    while(fscanf(fp,"%[^#]#%[^#]#%d#%d\n", tempFoglalas.azonosito, tempFoglalas.nev, &(tempFoglalas.ulohely), &(tempFoglalas.menu)) != EOF) {
+        foglalasokMeret++;
+        if(foglalasokMeret == 1) {
+            foglalasok = (Foglalas*)malloc(foglalasokMeret * sizeof(*foglalasok));
+        } else {
+            foglalasok = (Foglalas*)realloc(foglalasok, foglalasokMeret * sizeof(*foglalasok));
+        }
+        foglalasok[i] = tempFoglalas;
+        for(int j = 0; j < jaratokMeret; j++) {
+            if(strcmp(jaratok[j].azonosito, foglalasok[i].azonosito) == 0) {
+                if(jaratok[j].foglaltUlesek == NULL) {
+                    jaratok[j].foglaltUlesek = (int*)malloc(((96-jaratok[j].ferohely)+1) * sizeof(int));
+                } else {
+                    jaratok[j].foglaltUlesek = realloc(jaratok[j].foglaltUlesek,((96-jaratok[j].ferohely)+1) * sizeof(int));
+                }
+                jaratok[j].ferohely--;
+            }
+        }
+        i++;
+    }
+    fclose(fp);
+
+
+    //printf("%s %d %d %d %s %s %d\n", jaratok[0].azonosito, jaratok[0].datum.ev, jaratok[0].datum.honap, jaratok[0].datum.nap, jaratok[0].honnan, jaratok[0].hova, jaratok[0].ferohely);
 
 
 
 
 
 
-    /* menü */
+
+
+
+
+    /* Menü */
 
     int menupont = 0;
 
@@ -127,7 +181,9 @@ int main() {
         printf("5.: Kilépés\n");
         scanf("%d",&menupont);
 
+        /* Járat keresése menüpont */
         if(menupont == 1) {
+            /* Úticélok beolvasása */
             fflush(stdin);
             printf("Honnan kíván utazni?");
             char honnan[50];
@@ -136,27 +192,54 @@ int main() {
             char hova[50];
             gets(hova);
 
+            /* Dátumok beolvasása */
             printf("Mi legyen az indulási dátum?");
-            Datum idopont_kezdo;
-            scanf("%d/%d/%d", &(idopont_kezdo.ev), &(idopont_kezdo.honap), &(idopont_kezdo.nap));
-            while(hibaKeres(idopont_kezdo)) {
+            Datum datum_kezdo = datumBeolvas();
+            while(hibaKeres(datum_kezdo)) {
                 printf("Hibás Dátum formátum, kérlek próbáld újra\n");
-                scanf("%d/%d/%d", &(idopont_kezdo.ev), &(idopont_kezdo.honap), &(idopont_kezdo.nap));
+                printf("Mi legyen az indulási dátum?");
+                datum_kezdo = datumBeolvas();
             }
-            Datum idopont_vegso;
+            Datum datum_vegso = datumBeolvas();
             printf("Mi legyen a végsõ dátum?");
-            scanf("%d/%d/%d", &(idopont_vegso.ev), &(idopont_vegso.honap), &(idopont_vegso.nap));
-            while(hibaKeres(idopont_vegso)) {
+            while(hibaKeres(datum_vegso)) {
                 printf("Hibás Dátum formátum, kérlek próbáld újra\n");
-                scanf("%d/%d/%d", &(idopont_vegso.ev), &(idopont_vegso.honap), &(idopont_vegso.nap));
+                printf("Mi legyen a végsõ dátum?");
+                datum_vegso = datumBeolvas();
             }
-            jaratKeres(jaratok, jaratokMeret, honnan, hova, idopont_kezdo, idopont_vegso);
+
+            /* Járat keresése */
+            jaratKeres(jaratok, jaratokMeret, honnan, hova, datum_kezdo, datum_vegso);
         }
 
+        /* Repülõjegy foglalása menüpont */
+        else if(menupont == 2) {
+            printf("Ez a repülõjegy foglalása menüpont.\n");
+        }
+
+        /* Foglalás törlése menüpont */
+        else if(menupont == 3) {
+            printf("Foglalás törlése.\n");
+        }
+
+        /* Összesítés menüpont */
+        else if(menupont == 4) {
+            printf("Összesítés.\n");
+        }
+
+        /* Minden más érték az 5-öt kivéve => nem létezõ menüpont */
+        else if(menupont != 5) {
+            printf("Nem létezõ menüpont.\n");
+        }
 
     }
 
-    fclose(fp);
+
+
+
+
+
     free(jaratok);
+    free(foglalasok);
     return 0;
 }
